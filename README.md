22c5cc556edc54405edb8e70f2bd86554979c534
Ramo <mramazant@gmail.com>
2023-01-10 19:59:01 -0700

    created html file, worked on index.js and created readme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Team-Profile-Generator
+
+## Project Description
+
+This application allows you to easily enter the basic information of the employees and create an html page. After categorizing your employees as Manager, Engineer and Intern, you need to enter their information in order and just end the program.
+
+![Here is a screeshot of the application:](./dist/img/ScreenShot.png)
+
+Here is the video of the application: https://drive.google.com/file/d/1W38_zmh2YhyGUiNoMr-Kd4be1icUoumw/view
--- index.js
+++ index.js
@@ -1,92 +1,188 @@
 //decleration
-const getGeneratedHTML = require("./src/html-template")
-const inquirer = require("inquirer")
-const fs = require("fs")
-
-
-inquirer.prompt ([
-    {
-    type: "input",
-    message: "What is manager's name?",
-    name: "managerName"
-},
-{
-    type: "input",
-    message: "What is manager's ID?",
-    name: "managerID"  
-},
-    {
-    type: "input",
-    message: "What is manager's e-mail?",
-    name: "managerMail"
-},
-    {
-    type: "input",
-    message: "What is manager's office number?",
-    name: "managerOffice"
-},
-    {
-    type: "list",
-    choices : ["Engineer", "Intern", "Finish building my team"],
-    message: "Select an option to add member",
-    name: "selectMember"
-},
-    {
-    type: "input",
-    message: "What is engineer's name?",
-    name: "engineerName"
-},
-    {
-    type: "input",
-    message: "What is engineer's ID?",
-    name: "engineerID"
-},
-    {
-    type: "input",
-    message: "What is engineer's e-mail?",
-    name: "egnineerMail"
-},
-    {
-    type: "input",
-    message: "What is engineer's GitHub?",
-    name: "engineerGitHub"
-},
-    {
-    type: "input",
-    message: "What is inern's name?",
-    name: "internName"
-},
-    {
-    type: "input",
-    message: "What is inern's ID?",
-    name: "internID"
-},
-    {
-    type: "input",
-    message: "What is intern's e-mail?",
-    name: "internMail"
-},
-    {
-    type: "input",
-    message: "What is intern's GitHub?",
-    name: "internGitHub"
-}
-])
-.then ((response) => {
-    console.log(response)
+const getGeneratedHTML = require("./src/html-template");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+const teamMembers = [];
+//main menu
+selectTeamMembers = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        choices: ["Engineer", "Intern", "Finish building my team"],
+        message: "Select an option to add member",
+        name: "selectMember",
+      },
+    ])
+    .then((data) => {
+      switch (data.selectMember) {
+        case "Engineer":
+          selectEngineer();
+          break;
+        case "Intern":
+          selectIntern();
+          break;
+        default:
+          finishBuildingTeam();
+          break;
+      }
+    });
+};
+//selecting engineer
+selectEngineer = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is engineer's name?",
+        name: "engineerName",
+      },
+      {
+        type: "input",
+        message: "What is engineer's ID?",
+        name: "engineerID",
+      },
+      {
+        type: "input",
+        message: "What is engineer's e-mail?",
+        name: "engineerMail",
+      },
+      {
+        type: "input",
+        message: "What is engineer's GitHub?",
+        name: "engineerGitHub",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      const engineer = new Engineer(
+        data.engineerName,
+        data.engineerID,
+        data.engineerMail,
+        data.engineerGitHub
+      );
+      console.log(engineer.getRole());
+      teamMembers.push(engineer);
+      selectTeamMembers();
+    });
+};
+//selecting Intern
+selectIntern = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is intern's name?",
+        name: "internName",
+      },
+      {
+        type: "input",
+        message: "What is intern's ID?",
+        name: "internID",
+      },
+      {
+        type: "input",
+        message: "What is intern's e-mail?",
+        name: "internMail",
+      },
+      {
+        type: "input",
+        message: "What is intern's School?",
+        name: "internSchool",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      const intern = new Intern(
+        data.internName,
+        data.internID,
+        data.internMail,
+        data.internSchool
+      );
+      console.log(intern.getRole());
+      teamMembers.push(intern);
+      selectTeamMembers();
+    });
+};
+//selecting Manager
+selectManager = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is manager's name?",
+        name: "managerName",
+      },
+      {
+        type: "input",
+        message: "What is manager's ID?",
+        name: "managerID",
+      },
+      {
+        type: "input",
+        message: "What is manager's e-mail?",
+        name: "managerMail",
+      },
+      {
+        type: "input",
+        message: "What is manager's office number?",
+        name: "managerOffice",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      const manager = new Manager(
+        data.managerName,
+        data.managerID,
+        data.managerMail,
+        data.managerOffice
+      );
+      console.log(manager.getRole());
+      teamMembers.push(manager);
+      selectTeamMembers();
+    });
+};
+selectManager();
+//to ending the application
+finishBuildingTeam = () => {
+  let output = "";
+  console.log(teamMembers);
+  teamMembers.forEach((employee, index) => {
+    console.log(employee.getName(), employee.getRole());
+    //for non-common information
+    var additionalInfo = ``;
+    switch (employee.getRole()) {
+      case "Engineer":
+        additionalInfo = `<div class="addInfo"><h3>Github: ${employee.getGitHub()}</h3></div>`;
+        break;
+      case "Intern":
+        additionalInfo = `<div class="addInfo"><h3>School: ${employee.getSchool()}</h3></div>`;
+        break;
+      case "Manager":
+        additionalInfo = `<div class="addInfo"><h3>OfficeNumber: ${employee.officeNumber}</h3></div>`;
+        break;
+    }
+    //context of html file
     var memberInfo = `<div class="memberInfo">
-            <div class="managerName"></div>
-            <div class="managerID"><h1>Intern ID: ${response.internID}</h1></div>
-            <div class="managerMail"></div>
-            <div class="managerOffice"></div>
-        </div>`
-    //change file to dist/index.html
-    fs.writeFile("output/team.html", getGeneratedHTML(internID), (error) =>
-    error ? console.log(error) : console.log("Succesfull"));
-})
-
-
-
-
-
-
+            <div class="memberRole"><h1>${employee.getRole()}</h1></div>
+            <div class="memberName"><h3>Name: ${employee.getName()}</h3></div>
+            <div class="memberID"><h3>ID: ${employee.getId()}</h3></div>
+            <div class="memberMail"><h3>Mail: ${employee.getEmail()}</h3></div>
+            ${additionalInfo}
+        </div>`;
+    output += memberInfo;
+  });
+  console.log(output);
+  processHTML(output);
+};
+//creating html file
+processHTML = (memberInfo) => {
+  //change file to dist/index.html
+  fs.writeFile("dist/index.html", getGeneratedHTML(memberInfo), (error) =>
+    error ? console.log(error) : console.log("Succesfull")
+  );
+};
